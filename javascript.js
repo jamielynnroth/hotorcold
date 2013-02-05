fe061677508648b36c4a8addb72744d6563a2f50
Jamie Roth <roth.j@me.com>
2013-02-05 00:12:47 -0500

    Fix css positioning issues, add again animation

--- javascript.js
+++ javascript.js
@@ -1,62 +1,63 @@
 // // Randomly select a number between 0 and 50
-
-
-// // Ask the user to pick a number when they click the guess button
-
-
-
-
-
-
 $(document).ready(function(){
 var number= Math.floor(Math.random()*101);
 
 	$('#controls').submit(function(event){
 		event.preventDefault();
 		var guess = $("#controls input").val();
-		// alert(guess);
-		// var guess = prompt("Guess a number:");
+		// console.log(guess);
 		playGame(guess, number);
 	}); // click event
+	
+	$('#play_again').click(function() {
+			location.reload();
+		}); //#play_again button
 }); //doc ready
 
-// Check if the guess is correct and give feedback 
+
+
+// // Check if the guess is correct
 var playGame = function (guess, number){
 	if (!guess){
 		return ; 
 	}else if (guess == number){
+		// console.log('got it');
 		$('#correct').show();
+		$('#correct').html(number +'! Yahtzee!'+ '<form><input type="button" id="play_again" value="Play Again!"></form>');
 		$('#high').hide();
 		$('#low').hide();
 		$('#again').hide();
 		$('#controls').hide();
-		$('#correct').html(number+'! Yatzee!'+ '<form><input type="button" id="play_again" value="Play Again!"></form>');
 	} else if (guess > number){
-		$('#again').show();
+		// console.log('too high');
+		$('#again').fadeToggle('slow', 'linear');
 		$('#high').show();
 		$('#low').hide();
 		$('#correct').hide();
-		// guess = $("#controls option:selected").val();
-		// playGame(guess, number);
+		$('#again').fadeToggle('slow', 'linear');
+		// var lastGuess= guess;
+		// if (lastGuess-number < guess-number){
+		// 	console.log("Getting warmer!");
+		// }else if (lastGuess-numbe > guess-number) {
+		// 	console.log("Brrrr...");
+		// };
 	}else if (guess < number){
-		$('#again').show();
+		// console.log('too low');
+		$('#again').fadeToggle('slow', 'linear');
 		$('#low').show();
 		$('#high').hide();
 		$('#correct').hide();
-		// guess = $("#controls option:selected").val();
-		// playGame(guess, number);
-	}
-	// else {
-	// 	alert("That's not a number...")
-	// 	guess = $("#controls option:selected").val();
-	// 	prompt('Guess again!');
-	// 	// playGame(guess, number);
-	// }
+		$('#again').fadeToggle('slow', 'linear');
+		// lastGuess= guess;
+		// if (lastGuess-number < guess-number){
+		// 	console.log("Getting warmer!");
+		// }else if (lastGuess-numbe > guess-number) {
+		// 	console.log("Brrrr...");
+		// };
+	};
 };
 
-$('#play_again').click(function() {
-	location.reload();
-	});
+
 
 
 
---
+++ javascript2.js
@@ -0,0 +1,61 @@
+// // Randomly select a number between 0 and 50
+
+
+// // Ask the user to pick a number when they click the guess button
+
+
+
+
+
+
+$(document).ready(function(){
+var number= Math.floor(Math.random()*101);
+
+	$('#controls').submit(function(event){
+		event.preventDefault();
+		var guess = $("#controls input").val();
+		// alert(guess);
+		// var guess = prompt("Guess a number:");
+		playGame(guess, number);
+	}); // click event
+}); //doc ready
+
+// Check if the guess is correct and give feedback 
+var playGame = function (guess, number){
+	if (!guess){
+		return ; 
+	}else if (guess == number){
+		$('#correct').show();
+		$('#high').hide();
+		$('#low').hide();
+		$('#again').hide();
+		$('#controls').hide();
+		$('#correct').html(number+'! Yatzee!'+ '<form><input type="button" id="play_again" value="Play Again!"></form>');
+	} else if (guess > number){
+		$('#again').show();
+		$('#high').show();
+		$('#low').hide();
+		$('#correct').hide();
+		// guess = $("#controls option:selected").val();
+		// playGame(guess, number);
+	}else if (guess < number){
+		$('#again').show();
+		$('#low').show();
+		$('#high').hide();
+		$('#correct').hide();
+		// guess = $("#controls option:selected").val();
+		// playGame(guess, number);
+	}
+	// else {
+	// 	alert("That's not a number...")
+	// 	guess = $("#controls option:selected").val();
+	// 	prompt('Guess again!');
+	// 	// playGame(guess, number);
+	// }
+};
+
+$('#play_again').click(function() {
+	location.reload();
+	});
+
+
